649056f24d24c2b69e09450b23d5307dbdbf836f
Laoyang <6874302@qq.com>
2024-06-23 17:57:34 +0800

    pycharm快捷键

---
+++ kuaijiejian.py
@@ -0,0 +1,30 @@
+# 常用得快捷键
+# 注释ctral+/
+# 代码右移：tab
+# 代码左移： shift+tab
+# 自动缩进：ctral+alt+I
+# 运行:shift+ctral+F10
+# pe8规范格式:ctral+alt+l
+# 查找ctral+F
+# 替换:Ctral+R
+# 光标移到下一行shift+enter
+# 删除一行: ctral+y
+# 复制一行:ctral+D
+# 移动行:shift+alt+箭头
+# 设置：ctral+alt+s
+# 选中一个对象:ctral+w
+# 多行光标点击：alt+鼠标左键点击
+
+
+print("hello 老杨")
+# iter 快捷方式
+s = 'python'
+for i in s:
+    print(i)
+
+l = [i for i in s]
+print(l)
+
+# coml 提高效率
+l = [i for i in s]
+print(l)
